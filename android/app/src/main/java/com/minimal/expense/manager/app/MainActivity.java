f6fec7f7a50bb53999529a70499dbb881851eb11
Umesh Sevra <sevra@atb-bremen.de>
2024-07-03 19:24:39 +0200

    android package added

---
+++ android/app/src/main/java/com/minimal/expense/manager/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.minimal.expense.manager.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.minimal.expense.manager.app',
+  appName: 'minimal-expense-manager',
+  webDir: 'www'
+};
+
+export default config;
